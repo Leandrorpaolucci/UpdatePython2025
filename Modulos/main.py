10d6742f5ebfadd542490fcbb5e34a572e2e310d
Leandroydc <leandroydc2@gmail.com>
2025-02-04 23:27:40 -0300

    #5
    #5

---
+++ Modulos/main.py
@@ -0,0 +1,17 @@
+# Programa Principal
+
+import math_operations
+from math_operations import multiply, divide
+import string_utils
+
+print(math_operations.sum(5,3)) # soma
+print(math_operations.subtract(5,3)) #suvtrair
+print(multiply(5,3)) #multiplicar
+print(divide(5,3)) # dividir
+
+print(string_utils.capitalize('Hello'))
+print(string_utils.reverse_string('Python'))
+print(string_utils.count('Apple'))
+
+
+
---
+++ Modulos/math_operations.py
@@ -0,0 +1,16 @@
+# Modulo de operações matematicas
+
+def sum(x, y):
+    return x + y
+
+def subtract(x, y):
+    return x - y
+
+def multiply(x, y):
+    return x * y
+
+def  divide(x, y):
+    if y != 0:
+        return x / y
+    else:
+        print('Não é possivel dividir por zero')
